f8c1dfa69db71f103a97fb5599974f881a8d7a16
kayaru <istorytale090415@gmail.com>
2020-04-07 23:47:46 +0900

    add edge line and cleare primary node

--- graphviz_insert/A002_aci_structure.py
+++ graphviz_insert/A002_aci_structure.py
@@ -13,6 +13,7 @@ label           = ""
 
 KEY_CLUSTER = "CLUSTER"
 KEY_NODE = "NODE"
+KEY_EDGE = "EDGE"
 
 global data
 global row
@@ -24,26 +25,46 @@ def updateData():
     global compornent
     global aci_id
     global label
-    if(row < ROW_MAX):
+    if(row<ROW_MAX):
         compornent  = data[row][COL_COMPORNENT]
         aci_id      = data[row][COL_ID]
         label       = data[row][COL_NAME]
         row = row + 1
         return True
     else:
+        compornent  = ""
+        aci_id      = ""
+        label       = ""
         return False
 
+def isUnderLayer(cluster_id):
+    return len(aci_id)>len(cluster_id)
+
+def isEdge():
+    return compornent==KEY_EDGE
+
+def isNotEdge():
+    if isEdge():
+        return False
+    else:
+        return True
+
+def isNotEOF():
+    if compornent == "" :
+        return False
+    else:
+        return True
 
 def create_subgraph(uplayer):
     print(aci_id)
     #print(label)
-    name = 'cluster_' + aci_id + label
+    name = 'cluster_' + label
     cluster_id = aci_id
     with uplayer.subgraph(name=name) as cluster:
         cluster.attr(label=label)
-        cluster.node(name=label,label=label)
+        cluster.node(name=label,label="",color="white",height = "0.001",width ="0.001")
         updateData()
-        while (row < ROW_MAX and len(aci_id)>len(cluster_id)):
+        while (isUnderLayer(cluster_id) and isNotEdge() and isNotEOF()):
             if compornent == KEY_CLUSTER:
                 create_subgraph(cluster)
             elif compornent == KEY_NODE:
@@ -51,6 +72,20 @@ def create_subgraph(uplayer):
                 #print(name)
                 cluster.node(name=name,label=label)
                 updateData()
+
+def create_edge(dot):
+    print(isNotEOF())
+    print(isEdge())
+    while (isNotEOF() and isEdge()):
+        source_node = aci_id
+        dest_node   = label
+        print(source_node + " -> " + dest_node)
+        para_lhead  = 'cluster_' + dest_node
+        para_ltail  = 'cluster_' + source_node
+        dot.edge(source_node,dest_node,lhead=para_lhead,ltail=para_ltail)
+        updateData()
+
+                
 
 if __name__ == "__main__":
     
@@ -69,7 +104,10 @@ if __name__ == "__main__":
 
     if updateData():
         dot = Digraph(format='png')
+        dot.attr(compound="true")
+        dot.attr(bgcolor="white")
         create_subgraph(dot)
+        create_edge(dot)
         print(dot)
         dot.render("A002_aci_structure")
 
